e60dead3b0775e62118be2ad3b3d129b3da0b32d
Stellaros <chambonniere.paul1033@gmail.com>
2022-03-10 14:36:55 +0100

    Initial Commit

---
+++ commandes/index.php
@@ -0,0 +1,196 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>FuzeBot | MGC</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Site de FuzeBot développé par MythiGame Community" />
+	<meta name="author" content="Stellaros" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content="FuzeBot | MGC"/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content="https://fuzebot.mythigame.net/"/>
+	<meta property="og:site_name" content="FuzeBot | MGC"/>
+	<meta property="og:description" content="Site de FuzeBot développé par MythiGame Community"/>
+	<meta name="twitter:title" content="FuzeBot | MGC" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="https://fuzebot.mythigame.net/" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+	<link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="/css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="/css/flexslider.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="/css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="/js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	<div id="fh5co-page">
+		<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+		<aside id="fh5co-aside" role="complementary" class="border js-fullheight">
+
+			<h1 id="fh5co-logo"><a href="/">FuzeBot</a></h1>
+			<nav id="fh5co-main-menu" role="navigation">
+				<ul>
+					<li><a href="/">Accueil</a></li>
+					<li><a class="fh5co-active" href="/commandes/">Commandes</a></li>
+				</ul>
+			</nav>
+
+			<div class="fh5co-footer">
+				<p><small>&copy; 2021 <a href="https://www.mythigame.net/">MythiGame Community</a>. Tous droits réservés.</span></small></p>
+				<ul>
+					<li><a href="https://discord.gg/Jkmyj5U"><i class="fab fa-discord"></i></a></li>
+				</ul>
+			</div>
+
+		</aside>
+
+		<div id="fh5co-main">
+			<div class="fh5co-narrow-content">
+					<h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">Nos commandes</h2>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+								<div class="fh5co-icon">
+									<i class="icon-code"></i>
+								</div>
+								<div class="fh5co-text">
+									<h3>Help</h3>
+									<p>Permet d'afficher la liste de commandes. </p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+								<div class="fh5co-icon">
+									<i class="icon-code"></i>
+								</div>
+								<div class="fh5co-text">
+									<h3>Ban <(user)> [raison]</h3>
+									<p>Permet de bannir un membre. </p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+								<div class="fh5co-icon">
+									<i class="icon-code"></i>
+								</div>
+								<div class="fh5co-text">
+									<h3>Kick <(user)> [raison]</h3>
+									<p>Permet d'expulser un membre de façon temporaire. </p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+								<div class="fh5co-icon">
+									<i class="icon-code"></i>
+								</div>
+								<div class="fh5co-text">
+									<h3>Clear <(0-100)></h3>
+									<p>Permet de supprimer des messages. </p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+								<div class="fh5co-icon">
+									<i class="icon-code"></i>
+								</div>
+								<div class="fh5co-text">
+									<h3>Warn <(user)> [raison]</h3>
+									<p>Permet d'avertir un membre. </p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+								<div class="fh5co-icon">
+									<i class="icon-code"></i>
+								</div>
+								<div class="fh5co-text">
+									<h3>Unwarn <(user)> <(ID)></h3>
+									<p>Permet de supprimer un avertissement à un membre donné. </p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+								<div class="fh5co-icon">
+									<i class="icon-code"></i>
+								</div>
+								<div class="fh5co-text">
+									<h3>Infractions <(user)></h3>
+									<p>Permet de consulter les sanctions d'un membre. </p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div id="get-in-touch">
+				<div class="fh5co-narrow-content">
+					<div class="row">
+						<div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+							<h1 class="fh5co-heading-colored">Où nous trouver ?</h1>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<p class="fh5co-lead">Le discord support vous permet de rester informé. Nous sommes actuellement à la recherche de supports et modérateurs.</p>
+							<p><a href="https://discord.gg/Jkmyj5U" class="btn btn-primary">Rejoindre notre discord</a></p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- jQuery -->
+	<script src="/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="/js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="/js/jquery.flexslider-min.js"></script>
+	
+	
+	<!-- MAIN JS -->
+	<script src="/js/main.js"></script>
+
+	</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,245 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>FuzeBot | MGC</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Site de FuzeBot développé par MythiGame Community" />
+	<meta name="author" content="Stellaros" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content="FuzeBot | MGC"/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content="https://fuzebot.mythigame.net/"/>
+	<meta property="og:site_name" content="FuzeBot | MGC"/>
+	<meta property="og:description" content="Site de FuzeBot développé par MythiGame Community"/>
+	<meta name="twitter:title" content="FuzeBot | MGC" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="https://fuzebot.mythigame.net/" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	<div id="fh5co-page">
+		<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+		<aside id="fh5co-aside" role="complementary" class="border js-fullheight">
+
+			<h1 id="fh5co-logo"><a href="/">FuzeBot</a></h1>
+			<nav id="fh5co-main-menu" role="navigation">
+				<ul>
+					<li><a href="/">Accueil</a></li>
+					<li><a class="fh5co-active" href="/commandes/">Commandes</a></li>
+				</ul>
+			</nav>
+
+			<div class="fh5co-footer">
+				<p><small>&copy; 2021 <a href="https://www.mythigame.net/">MythiGame Community</a>. Tous droits réservés.</span></small></p>
+				<ul>
+					<li><a href="https://discord.gg/Jkmyj5U"><i class="fab fa-discord"></i></a></li>
+				</ul>
+			</div>
+
+		</aside>
+
+		<div id="fh5co-main">
+			<aside id="fh5co-hero" class="js-fullheight">
+				<div class="flexslider js-fullheight">
+					<ul class="slides">
+				   	<li style="background-image: url(images/background.jpg);">
+				   		<div class="overlay"></div>
+				   		<div class="container-fluid">
+				   			<div class="row">
+					   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+					   				<div class="slider-text-inner">
+					   					<h1><strong>FuzeBot</strong></h1>
+					   					<h2>Un robot discord nouvelle génération</h2>
+											<p><a class="btn btn-primary btn-demo popup-vimeo" href="https://discord.com/oauth2/authorize?client_id=834065649411293215&scope=bot&permissions=8"> <i class="icon-monitor"></i> Invitez FuzeBot</a> <a class="btn btn-primary btn-learn" href="/commandes/">En savoir plus<i class="icon-arrow-right3"></i></a></p>
+					   				</div>
+					   			</div>
+					   		</div>
+				   		</div>
+				   	</li>
+				   	<!-- <li style="background-image: url(images/img_bg_2.jpg);">
+				   		<div class="overlay"></div>
+				   		<div class="container-fluid">
+				   			<div class="row">
+					   			<div class="col-md-8 col-md-offset-2 text-center js-fullheight slider-text">
+					   				<div class="slider-text-inner">
+					   					<h1>We are Happy to Create Newest Modern Websites</h1>
+											<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+											<p><a class="btn btn-primary btn-demo popup-vimeo" href="https://vimeo.com/channels/staffpicks/93951774"> <i class="icon-monitor"></i> Live Preview</a> <a class="btn btn-primary btn-learn">Learn More<i class="icon-arrow-right3"></i></a></p>
+					   				</div>
+					   			</div>
+					   		</div>
+				   		</div>
+				   	</li> -->
+				  	</ul>
+			  	</div>
+			</aside>
+			
+			<!--<div class="fh5co-narrow-content">
+				<h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">Services</h2>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+							<div class="fh5co-icon">
+								<i class="icon-settings"></i>
+							</div>
+							<div class="fh5co-text">
+								<h3>Strategy</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+							<div class="fh5co-icon">
+								<i class="icon-search4"></i>
+							</div>
+							<div class="fh5co-text">
+								<h3>Explore</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6">
+						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+							<div class="fh5co-icon">
+								<i class="icon-paperplane"></i>
+							</div>
+							<div class="fh5co-text">
+								<h3>Direction</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="fh5co-feature animate-box" data-animate-effect="fadeInLeft">
+							<div class="fh5co-icon">
+								<i class="icon-params"></i>
+							</div>
+							<div class="fh5co-text">
+								<h3>Expertise</h3>
+								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="fh5co-narrow-content">
+				<h2 class="fh5co-heading animate-box" data-animate-effect="fadeInLeft">Recent Blog</h2>
+				<div class="row row-bottom-padded-md">
+					<div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
+						<div class="blog-entry">
+							<a href="#" class="blog-img"><img src="images/img-1.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+							<div class="desc">
+								<h3><a href="#">Inspirational Website</a></h3>
+								<span><small>by Admin </small> / <small> Web Design </small> / <small> <i class="icon-comment"></i> 14</small></span>
+								<p>Design must be functional and functionality must be translated into visual aesthetics</p>
+								<a href="#" class="lead">Read More <i class="icon-arrow-right3"></i></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
+						<div class="blog-entry">
+							<a href="#" class="blog-img"><img src="images/img-2.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+							<div class="desc">
+								<h3><a href="#">Inspirational Website</a></h3>
+								<span><small>by Admin </small> / <small> Web Design </small> / <small> <i class="icon-comment"></i> 14</small></span>
+								<p>Design must be functional and functionality must be translated into visual aesthetics</p>
+								<a href="#" class="lead">Read More <i class="icon-arrow-right3"></i></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
+						<div class="blog-entry">
+							<a href="#" class="blog-img"><img src="images/img-3.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+							<div class="desc">
+								<h3><a href="#">Inspirational Website</a></h3>
+								<span><small>by Admin </small> / <small> Web Design </small> / <small> <i class="icon-comment"></i> 14</small></span>
+								<p>Design must be functional and functionality must be translated into visual aesthetics</p>
+								<a href="#" class="lead">Read More <i class="icon-arrow-right3"></i></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
+						<div class="blog-entry">
+							<a href="#" class="blog-img"><img src="images/img-4.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by FreeHTML5.co"></a>
+							<div class="desc">
+								<h3><a href="#">Inspirational Website</a></h3>
+								<span><small>by Admin </small> / <small> Web Design </small> / <small> <i class="icon-comment"></i> 14</small></span>
+								<p>Design must be functional and functionality must be translated into visual aesthetics</p>
+								<a href="#" class="lead">Read More <i class="icon-arrow-right3"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div id="get-in-touch">
+				<div class="fh5co-narrow-content">
+					<div class="row">
+						<div class="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+							<h1 class="fh5co-heading-colored">Get in touch</h1>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+							<p class="fh5co-lead">Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							<p><a href="#" class="btn btn-primary">Learn More</a></p>
+						</div>
+
+					</div>
+				</div>
+			</div>-->
+		</div>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	
+	
+	<!-- MAIN JS -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
